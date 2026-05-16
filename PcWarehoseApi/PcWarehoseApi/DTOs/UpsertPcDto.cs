b953cae1abfc2c524da6823dac0cc4f29a8bc3bd
Zlata Oleksiuk <s21168@pjwstk.edu.pl>
2026-05-16 16:49:02 +0200

    Add dto for creating and updating pcs

---
+++ PcWarehoseApi/PcWarehoseApi/DTOs/UpsertPcDto.cs
@@ -0,0 +1,10 @@
+namespace PcWarehoseApi.DTOs;
+
+public class UpsertPcDto
+{
+    public string Name { get; set; } = string.Empty;
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+}
